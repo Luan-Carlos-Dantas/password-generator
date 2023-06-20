c09490a3bc9e6f32992fe37ded88dd91f479e4fe
Luan-Carlos-Dantas <109487706+Luan-Carlos-Dantas@users.noreply.github.com>
2023-06-20 03:29:53 -0300

    Adicionando responsividadem e respostas visuais para o usuário

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,15 +42,62 @@ function generatePassword() {
 }
 
 function calculateQuality() {
-  const percent = Math.round((passwordLength / 64) * 100);
+  //length * peso1 + uppercase * peso2 + number * peso3 + symbol * peso4
+  const percent = Math.round(
+    (passwordLength / 64) * 25 +
+      (uppercaseCheck.checked ? 15 : 0) +
+      (numberCheck.checked ? 25 : 0) +
+      (symbolCheck.checked ? 35 : 0)
+  );
+
+  if (percent > 69) {
+    indicatorBar.classList.remove("critical");
+    indicatorBar.classList.remove("warning");
+    indicatorBar.classList.add("safe");
+  } else if (percent > 50) {
+    indicatorBar.classList.add("warning");
+    indicatorBar.classList.remove("critical");
+    indicatorBar.classList.remove("safe");
+  } else {
+    indicatorBar.classList.add("critical");
+    indicatorBar.classList.remove("warning");
+    indicatorBar.classList.remove("safe");
+  }
+
+  if (percent >= 100) {
+    indicatorBar.classList.add("completed");
+  } else {
+    indicatorBar.classList.remove("completed");
+  }
 
   indicatorBar.style.width = `${percent}%`;
 
   console.log(percent);
 }
 
+function calculateFontSize() {
+  if (passwordLength > 45) {
+    textInput.classList.remove("font-xs");
+    textInput.classList.remove("font-sm");
+    textInput.classList.add("font-xxs");
+  } else if (passwordLength > 32) {
+    textInput.classList.add("font-xs");
+    textInput.classList.remove("font-sm");
+    textInput.classList.remove("font-xxs");
+  } else if (passwordLength > 22) {
+    textInput.classList.remove("font-xs");
+    textInput.classList.add("font-sm");
+    textInput.classList.remove("font-xxs");
+  } else {
+    textInput.classList.remove("font-xs");
+    textInput.classList.remove("font-sm");
+    textInput.classList.remove("font-xxs");
+  }
+}
+
 function copyPaste() {
   navigator.clipboard.writeText(textInput.value);
+  alert("Senha copiada para a área de transferência");
 }
 
 const passwordLengthEl = document.querySelector("#password-length");
@@ -60,6 +107,7 @@ passwordLengthEl.addEventListener("input", () => {
   lengthPassword.innerText = passwordLengthEl.value;
   console.log(passwordLength);
   generatePassword();
+  calculateFontSize();
 });
 
 uppercaseCheck.addEventListener("click", generatePassword);
@@ -71,5 +119,7 @@ btn.addEventListener("click", copyPaste);
 
 const btn_input = document.querySelector("#copyBtn-2");
 btn_input.addEventListener("click", copyPaste);
+
+document.querySelector("#reload").addEventListener("click", generatePassword);
 
 generatePassword();
